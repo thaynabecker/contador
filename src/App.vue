a0bd72097d45cac642d0fba5ac5fce8f95c9d4d8
thaynabecker <thaynabecker.ifcaraquari@gmail.com>
2025-03-24 11:52:48 -0300

    Contador

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+const contador = ref(0)
+
+function incrementar() {
+    contador.value++
+}
+function decrementar() {
+    contador.value--
+}
+</script>
+
+<template>
+ <h1>Meu contador</h1>
+ <p>Valor do contador: {{ contador }}</p>
+ <button v-on:click="incrementar">+</button>
+ <button @click="decrementar">-</button>
+</template>
+
+<style scoped>
+
+</style>
