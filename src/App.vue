72bcb8e9975c8c3fb7cc31272849732c8c1e7b9b
thaynabecker <thaynabecker.ifcaraquari@gmail.com>
2025-03-27 08:13:08 -0300

    Botão reiniciar adicionado.

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,16 @@ const contador = ref(0)
 function incrementar() {
     contador.value++
 }
-function decrementar() {
+function decrementar(){
+  if (contador.value > 0) {
     contador.value--
+  } else {
+    alert(`O contador não pode ser menor que 0!`)
+  }
+
+}
+function reiniciar(){
+  contador.value = 0
 }
 </script>
 
@@ -16,6 +24,7 @@ function decrementar() {
  <p>Valor do contador: {{ contador }}</p>
  <button v-on:click="incrementar">+</button>
  <button @click="decrementar">-</button>
+ <button @click="reiniciar">reiniciar</button>
 </template>
 
 <style scoped>
